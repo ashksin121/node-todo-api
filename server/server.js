6c3a7cee5804d6b1b5c9de26b2cba28109ff6356
ashksin121 <ashksin121@gmail.com>
2019-07-30 01:26:29 +0530

    Added POST /users/login route

--- server/server.js
+++ server/server.js
@@ -121,6 +121,18 @@ app.delete('/users/me/token', authenticate, (req, res) => {
     })
 })
 
+app.post('/users/login', (req, res) => {
+    var body = _.pick(req.body, ['email', 'password']);
+    User.findByCredentials(body.email, body.password).then((user) => {
+        // res.send(user);
+        return user.generateAuthToken().then((token) => {
+            res.header('x-auth', token).send(user);
+        })
+    }).catch((e) => {
+        res.status(400).send();
+    })
+})
+
 app.listen(port, () => {
     console.log(`Started at port ${port}`);
 })
